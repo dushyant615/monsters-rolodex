a80c5cb41936d933b3e440ea71334cc6f00b2ba4
dushyant615 <dushyant.bhavsar15@gmail.com>
2019-11-24 22:01:21 +0530

    Updates

---
+++ precache-manifest.d900f90e9868782a75f7347025ec051c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571073ce9aa518104c9fb366050c17df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3dcb179a84affe608ec4",
+    "url": "/monsters-rolodex/static/css/main.7f1c420c.chunk.css"
+  },
+  {
+    "revision": "efcdc4ea3e806355e770",
+    "url": "/monsters-rolodex/static/js/2.683e9fc3.chunk.js"
+  },
+  {
+    "revision": "3dcb179a84affe608ec4",
+    "url": "/monsters-rolodex/static/js/main.0743d1cc.chunk.js"
+  },
+  {
+    "revision": "c435118982faf650ab42",
+    "url": "/monsters-rolodex/static/js/runtime-main.bb3559cf.js"
+  }
+]);
